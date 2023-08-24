25e6980e19be9e4bfeee8fc9c4c808bf7bd5f713
Ganesh Baral <ganesh.baral@hotmail.co.uk>
2023-08-24 16:05:36 +0100

    profile changed

--- 00-labs/00-create-ec2/ec2.tf
+++ 00-labs/00-create-ec2/ec2.tf
@@ -19,9 +19,10 @@ resource "aws_instance" "web-server" {
   # ami         = "ami-047a51fa27710816e" # Supply manually
   instance_type = "t2.micro"
   availability_zone = "us-east-1a" #Optional, unless wanted to launch ec2 in specific zone
-
+  count = 2 # Use meta argument count if multiple resources needed
   tags = {
-    Name = "HelloWorld"
-    NewTag = "This is new tag"
+    Name = "web-server-${count.index}"
+    Enviornment = "Testing"
+    Owner = "ganesh.baral"
   }
 }
--- 00-labs/00-create-ec2/provider.tf
+++ 00-labs/00-create-ec2/provider.tf
@@ -2,8 +2,8 @@
 # Provider Block
 provider "aws" {
   region = "us-east-1"  
-  # profile = "default" 
-  profile = "sandbox"
+  profile = "default" 
+  # profile = "sandbox"
 }
 
 /*
